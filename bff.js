5812098a470f1793693d5d670fdbee9dd0093d84
Michael Claus <michaelclaus@Postmans-MacBook-Pro-5.local>
2021-05-05 15:27:25 -0700

    fetch YouTube data on build

--- bff.js
+++ bff.js
@@ -2,12 +2,14 @@ const sh = require('shelljs');
 const pingWebHook = require('./scripts/pingWebHook');
 const fetchBlogPosts = require('./scripts/fetchBlogPosts');
 const fetchEvents = require('./scripts/fetchEvents')
+const fetchYouTube = require('./scripts/fetchYouTube');
 
 const prefetch = async (dir, response) => {
   sh.exec('mkdir -p bff-data');
   await pingWebHook();
   fetchBlogPosts();
   fetchEvents();
+  fetchYouTube();
 };
 
 prefetch();
